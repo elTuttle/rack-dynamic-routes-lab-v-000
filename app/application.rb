8b5581a4d9a30eb2c9756c709e515b66ea5fc6ed
John Tuttle <thejwt@gmail.com>
2017-10-26 07:28:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,18 @@
 class Application
 
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Response.new
 
     if req.path.match(/items/)
+
+      item_name = req.path.split("/items/").last
+      item 
       
     end
-    
+
   end
 
 end
