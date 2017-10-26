5425234afd309a23b2fecfececf2bfb57d2b6b35
John Tuttle <thejwt@gmail.com>
2017-10-26 07:31:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,9 @@ class Application
       item = @@items.find{|i| i.name == item_name}
 
       resp.write item.price
-
+    else
+      resp.write "Route not found"
+      resp.status = 404  
     end
 
     resp.finish
